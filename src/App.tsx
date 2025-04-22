d6b0f45eb36245302f16afc6dff416278d85dfd5
Kris <kris.kajtna@gmail.com>
2025-04-22 21:59:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+
+function App(){
+  return(
+    <Routes>
+      <Route path="/"></Route>
+    </Routes>
+  ) 
+}
+
+export default App;
