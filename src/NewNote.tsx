1f0d62d9fc4a6ec6fa4894f5f4de156ed872a94a
Kris <kris.kajtna@gmail.com>
2025-04-22 22:45:40 +0200

    feat: add NoteForm component and integrate it into NewNote
    - Added a new NoteForm component for creating notes with title, tags, and body.
    - Integrated NoteForm into the NewNote component with a header.
    - Updated package.json to include react-select for tag input functionality.

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,5 +1,10 @@
+import { NoteForm } from "./NoteForm";
+
 export function NewNote(){
     return(
-        null
+       <>
+        <h1 className="mb-4">New Note</h1>
+        <NoteForm />
+       </>
     )
 }
