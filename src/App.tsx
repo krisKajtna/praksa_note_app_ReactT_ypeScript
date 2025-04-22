36ebea54136cf0e35b9c5f2218c5f74bbc1410eb
Kris <kris.kajtna@gmail.com>
2025-04-22 23:29:52 +0200

    feat: implement NoteForm component with form handling and local storage integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,34 @@ import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NewNote } from "./NewNote";
 
+export type Note= {
+  id: string
+}& NoteData
+
+export type RawNote = {
+  id: string
+}
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
 function App(){
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
   return(
     <Container className="my-4">
     <Routes>
